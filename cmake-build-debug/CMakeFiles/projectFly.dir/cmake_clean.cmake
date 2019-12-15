c9768d8080599c747bc081a28981bbea6523a22d
shiloleopold <shiloleopold@gmail.com>
2019-12-15 14:42:27 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/projectFly.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectFly.dir/main.cpp.o"
+  "projectFly"
+  "projectFly.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectFly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projectFly.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projectFly.
+# This may be replaced when dependencies are built.
