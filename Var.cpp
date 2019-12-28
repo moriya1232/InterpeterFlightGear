7d689c4750621e28e5b3a8b44adc880039b7c58c
shiloleopold <shiloleopold@gmail.com>
2019-12-28 21:58:13 +0200

    28.12 21:57

--- Var.cpp
+++ Var.cpp
@@ -4,14 +4,10 @@
 
 #include "Var.h"
 int Var :: execute(unordered_map <string,Command*>* mapCommand,vector<string>& data , int index,queue<string>*queueMas){
-    if (data[index] == "var"){
-
-        Var* v = new Var() ;
-
-    }
-    else{
-
-    }
+if(this->dir=='R') {
+    string str=this->sim.substr(1,(str.length()-1));
+    queueMas->push("set/"+str+ this->value);
+}
 }
 
 void Var::setDir(string s) {if (s == "->"){this->dir = 'R';}else{this->dir = 'L';}};
